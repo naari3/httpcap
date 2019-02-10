da1c077b7e1767c9e0d0084da299f84d5d69bdc9
naari3 <naari.named@gmail.com>
2019-02-11 06:00:39 +0900

    Split method

--- lib/httpcap.rb
+++ lib/httpcap.rb
@@ -11,20 +11,29 @@ require 'httpcap/response'
 module HTTPcap
   class Error < StandardError; end
 
+  def self.send_recv_combined_tcp_data(filepath)
+    Enumerator.new do |y|
+      ReassembleTcp.tcp_connections(filepath).map do |stream|
+        stream.tcpdata.each_slice(2) do |tcp_send, tcp_recv|
+          y << [tcp_send, tcp_recv]
+        end
+      end
+    end
+  end
+
   # get HTTP request and response from pcap file
   # @param [String] filepath pcapfile path
   # @return [Array<HTTPcap::HTTPConnection>] http connections
   def self.http_flows(filepath)
     Enumerator.new do |y|
-      ReassembleTcp.tcp_connections(filepath).map do |stream|
-        stream.tcpdata.each_slice(2) do |tcp_send, tcp_recv|
-          next unless tcp_send[1] == :send && tcp_recv[1] == :recv
+      send_recv_combined_tcp_data(filepath) do |tcp_send, tcp_recv|
+        recv, send = [tcp_send, tcp_recv].sort_by { |tcp| tcp[1] }
+        next unless send[1] == :send && recv[1] == :recv
 
-          request = Request.new(tcp_send[2])
-          response = Response.new(tcp_recv[2])
+        request = Request.new(send[2])
+        response = Response.new(recv[2])
 
-          y << HTTPFlow.new(request, response)
-        end
+        y << HTTPFlow.new(request, response)
       end
     end
   end
